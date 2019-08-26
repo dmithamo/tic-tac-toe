dae8491ecbddb6908ee681495745fa28429d454f
Dennis Mithamo <dennisbmithamo@gmail.com>
2019-08-26 12:21:34 +0300

    restructure the project to get rid of unnecessary files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Copyright from './components/Copyright';
+
+const App = () => (
+  <div>
+    <header>
+      <h1>Tic to the Tac Toe</h1>
+      <Copyright />
+    </header>
+  </div>
+);
+
+export default App;
---
+++ src/components/Copyright.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Copyright = () => <Container>by dmithamo</Container>;
+
+const Container = styled.h2`
+  background-color: grey;
+  color: white;
+  padding: 10px;
+  font-size: 12px;
+  border-radius: 5px;
+  width: 8%;
+  text-align: center;
+`;
+
+export default Copyright;
