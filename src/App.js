7c7626534eb0375be674c0a248e85d644059059d
Dennis Mithamo <dennisbmithamo@gmail.com>
2019-08-26 15:03:02 +0300

    set up the tictactoe table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Copyright from './components/Copyright';
+import Grid from './components/Grid';
 
 const App = () => (
   <div>
     <header>
       <h1>Tic to the Tac Toe</h1>
       <Copyright />
+      <Grid />
     </header>
   </div>
 );
