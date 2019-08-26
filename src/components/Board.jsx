f018b562fa1a422743742aeb99a4f8d3e8fd0e1b
Dennis Mithamo <dennisbmithamo@gmail.com>
2019-08-26 20:54:59 +0300

    add functionality to mark a box as appropraite when player plays

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,41 +1,112 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import styled from 'styled-components';
 
-class ClickableSquare extends Component {
-  // Needs to manage state of individual cell
-  render() {
-    return <StyledSquare />;
-  }
-}
+const ClickableSquare = (props) => {
+  const { value } = props;
+  return <StyledSquare onClick={props.onClick}>{value}</StyledSquare>;
+};
 
 class Board extends Component {
-  // Needs to manage state of entire board
+  constructor(props) {
+    super(props);
+    // Needs to manage state of entire board
+    this.state = {
+      squareValues: {
+        1: '',
+        2: '',
+        3: '',
+        4: '',
+        5: '',
+        6: '',
+        7: '',
+        8: '',
+        9: '',
+      },
+      xToPlay: true,
+    };
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(index) {
+    const { xToPlay } = this.state;
+    const currentValue = this.state.squareValues[index];
+    currentValue === ''
+      ? xToPlay
+        ? this.setState((prevState) => ({
+            ...prevState,
+            squareValues: { ...prevState.squareValues, [index]: 'X' },
+            xToPlay: false,
+          }))
+        : this.setState((prevState) => ({
+            ...prevState,
+            squareValues: { ...prevState.squareValues, [index]: 'O' },
+            xToPlay: true,
+          }))
+      : console.log('You kent play here');
+  }
+
   render() {
+    const { squareValues, xToPlay } = this.state;
     return (
-      <Table>
-        <tbody>
-          <Row />
-          <Row />
-          <Row />
-        </tbody>
-      </Table>
+      <Fragment>
+        <Subheader>{`${xToPlay ? 'X' : 'O'} to play`}</Subheader>
+        <Table>
+          <tbody>
+            <tr>
+              <ClickableSquare
+                onClick={() => this.onClick(1)}
+                value={squareValues[1]}
+              />
+              <ClickableSquare
+                onClick={() => this.onClick(2)}
+                value={squareValues[2]}
+              />
+              <ClickableSquare
+                onClick={() => this.onClick(3)}
+                value={squareValues[3]}
+              />
+            </tr>
+            <tr>
+              <ClickableSquare
+                onClick={() => this.onClick(4)}
+                value={squareValues[4]}
+              />
+              <ClickableSquare
+                onClick={() => this.onClick(5)}
+                value={squareValues[5]}
+              />
+              <ClickableSquare
+                onClick={() => this.onClick(6)}
+                value={squareValues[6]}
+              />
+            </tr>
+            <tr>
+              <ClickableSquare
+                onClick={() => this.onClick(7)}
+                value={squareValues[7]}
+              />
+              <ClickableSquare
+                onClick={() => this.onClick(8)}
+                value={squareValues[8]}
+              />
+              <ClickableSquare
+                onClick={() => this.onClick(9)}
+                value={squareValues[9]}
+              />
+            </tr>
+          </tbody>
+        </Table>
+      </Fragment>
     );
   }
 }
-
-const Row = () => (
-  <tr>
-    <ClickableSquare />
-    <ClickableSquare />
-    <ClickableSquare />
-  </tr>
-);
 
 const StyledSquare = styled.td`
   color: black;
   font-family: monospace;
   font-weight: bold;
-  font-size: 20px;
+  font-size: 80px;
   padding: 10px;
   text-align: center;
   width: 100px;
@@ -43,13 +114,18 @@ const StyledSquare = styled.td`
   cursor: pointer;
   border: 1px solid black;
   border-collapse: collapse;
-  width: 100px;
-  height: 100px;
 `;
 
 const Table = styled.table`
   border: 1px solid black;
   border-collapse: collapse;
+`;
+
+const Subheader = styled.div`
+  color: black;
+  font-family: monospace;
+  font-weight: bold;
+  font-size: 20px;
 `;
 
 export default Board;
