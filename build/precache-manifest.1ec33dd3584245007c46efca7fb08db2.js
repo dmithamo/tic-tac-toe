80847a37b6f39bcfdb98ec668cd0c5d2373f9665
Dennis Mithamo <dennisbmithamo@gmail.com>
2019-08-28 11:06:11 +0300

    deploy the app on github

---
+++ build/precache-manifest.1ec33dd3584245007c46efca7fb08db2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf360df30a24aab64c850df78856f6f",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "7b037f8411266202805c",
+    "url": "/tic-tac-toe/static/js/2.b829e5c8.chunk.js"
+  },
+  {
+    "revision": "d4eb5b9402f8bf6522b7",
+    "url": "/tic-tac-toe/static/js/main.70471da8.chunk.js"
+  },
+  {
+    "revision": "2447e331fa6994ee754e",
+    "url": "/tic-tac-toe/static/js/runtime~main.96c826cc.js"
+  }
+]);
